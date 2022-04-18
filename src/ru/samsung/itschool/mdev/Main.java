14d574fe2032cd1c05e6ccd2fd2c15059ca39b44
Pavel <impetus@yandex.ru>
2022-04-18 16:58:56 +0300

    Initial commit

---
+++ src/ru/samsung/itschool/mdev/Main.java
@@ -0,0 +1,26 @@
+package ru.samsung.itschool.mdev;
+
+import com.google.gson.Gson;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+class Staff {
+    public String name;
+    public int age;
+    public String position;
+    public BigDecimal salary;
+    public List<String> skills;
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        // GSON - Google SON (Script Object Notation)
+        String str = "{\"name\":\"Anton\",\"age\":15,\"position\":\"Junior\",\"salary\":100,\"skills\":[\"java\",\"xml\",\"python\",\"Android\"]}";
+        Gson gson = new Gson();
+        Staff staff = gson.fromJson(str, Staff.class);
+        System.out.println(staff.skills.size());
+
+    }
+}
